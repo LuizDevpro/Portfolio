0bbf7a8b861977c2fad0fb1d1170c2f555fafce9
LuizDevpro <luizdev.pro@gmail.com>
2026-07-26 02:32:03 -0300

    Responsividade do site feita, correções em layout, e menu na topbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,3 +15,38 @@ const observer = new IntersectionObserver(
     document.querySelectorAll('.hidden-section').forEach(section => {
       observer.observe(section);
     });
+
+// topbar
+const menuBtn = document.getElementById("menu-btn");
+const closeBtn = document.getElementById("close-menu");
+const mobileMenu = document.getElementById("mobile-menu");
+const overlay = document.getElementById("menu-overlay");
+
+function openMenu() {
+    mobileMenu.classList.remove("right-[-100%]");
+    mobileMenu.classList.add("right-0");
+
+    overlay.classList.remove("hidden");
+
+    document.body.style.overflow = "hidden";
+}
+
+function closeMenu() {
+    mobileMenu.classList.remove("right-0");
+    mobileMenu.classList.add("right-[-100%]");
+
+    overlay.classList.add("hidden");
+
+    document.body.style.overflow = "";
+}
+
+menuBtn.addEventListener("click", openMenu);
+
+closeBtn.addEventListener("click", closeMenu);
+
+overlay.addEventListener("click", closeMenu);
+
+// Fecha ao clicar em um link
+document.querySelectorAll("#mobile-menu a").forEach(link => {
+    link.addEventListener("click", closeMenu);
+}); 
