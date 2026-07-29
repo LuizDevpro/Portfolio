452461dbe79eacf29f6187ad9596c7cd7f359123
LuizDevpro <luizdev.pro@gmail.com>
2026-07-29 13:30:08 -0300

    Preparação para animações

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,20 +1,3 @@
-const observer = new IntersectionObserver(
-      (entries, observer) => {
-        entries.forEach(entry => {
-          if (entry.isIntersecting) {
-            entry.target.classList.add('show-section');
-            observer.unobserve(entry.target); 
-          }
-        });
-      },
-      {
-        threshold: 0.3  
-      }
-    );
-
-    document.querySelectorAll('.hidden-section').forEach(section => {
-      observer.observe(section);
-    });
 
 // topbar
 const menuBtn = document.getElementById("menu-btn");
